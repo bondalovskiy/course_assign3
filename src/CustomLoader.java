84b7eff723f42c0d6f795926f8e59f8c955a715c
Roma Bondalovskiy <bondalovskiyroma@gmail.com>
2025-03-10 16:25:11 +0300

    added caching using findLoadedClass(...) / remaking CustomLoader

--- src/CustomLoader.java
+++ src/CustomLoader.java
@@ -2,15 +2,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Map;
 
 public class CustomLoader extends ClassLoader {
 
     private final String classPath;
-
-    private final Map<String, Class<?>> loadedClasses = new HashMap<>();
-
 
     public CustomLoader(ClassLoader parent, String classPath){
         super(parent);
@@ -19,23 +14,21 @@ public class CustomLoader extends ClassLoader {
 
     @Override
     public Class<?> findClass(String name) {
-        System.out.println("Loading class " + name );
 
-        if (loadedClasses.containsKey(name)) {
+        Class<?> loadedClass = findLoadedClass(name);
+
+        if (loadedClass != null) {
             System.out.println("Class " + name + "was already loaded/ cached version returned");
-            return loadedClasses.get(name);
+            return loadedClass;
         }
+        System.out.println("Loading class " + name );
 
         try {
             String filePath = classPath + "/" + name.replace(".", "/") + ".class";
             System.out.println("PATH^" + filePath);
             byte[] classByte = Files.readAllBytes(Paths.get(filePath));
 
-            Class<?> customClass = defineClass(name, classByte, 0, classByte.length);
-
-            loadedClasses.put(name, customClass);
-
-            return customClass;
+            return defineClass(name, classByte, 0, classByte.length);
         } catch (IOException e) {
             throw new RuntimeException("Couldn't load " + name, e);
         }
