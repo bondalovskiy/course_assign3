9cddc3f550b5daadb5750f2b41672b60e7154cd9
Roma Bondalovskiy <bondalovskiyroma@gmail.com>
2025-03-10 16:09:26 +0300

    added caching / remaking CustomLoader

--- src/CustomLoader.java
+++ src/CustomLoader.java
@@ -2,10 +2,15 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CustomLoader extends ClassLoader {
 
     private final String classPath;
+
+    private final Map<String, Class<?>> loadedClasses = new HashMap<>();
+
 
     public CustomLoader(ClassLoader parent, String classPath){
         super(parent);
@@ -16,12 +21,21 @@ public class CustomLoader extends ClassLoader {
     public Class<?> findClass(String name) {
         System.out.println("Loading class " + name );
 
+        if (loadedClasses.containsKey(name)) {
+            System.out.println("Class " + name + "was already loaded/ cached version returned");
+            return loadedClasses.get(name);
+        }
+
         try {
             String filePath = classPath + "/" + name.replace(".", "/") + ".class";
             System.out.println("PATH^" + filePath);
             byte[] classByte = Files.readAllBytes(Paths.get(filePath));
 
-            return defineClass(name, classByte, 0, classByte.length);
+            Class<?> customClass = defineClass(name, classByte, 0, classByte.length);
+
+            loadedClasses.put(name, customClass);
+
+            return customClass;
         } catch (IOException e) {
             throw new RuntimeException("Couldn't load " + name, e);
         }
--- src/Main.java
+++ src/Main.java
@@ -10,11 +10,11 @@ public class Main {
         CustomLoader customLoader = new CustomLoader(Main.class.getClassLoader(), classPath);
 
         Class<?> customClass = customLoader.findClass("Custom1");
-
+        Class<?> customClass1 = customLoader.findClass("Custom1");
         System.out.println("Loaded class name: " + customClass.getName());
 
         Object customClassInstance = customClass.getDeclaredConstructor().newInstance();
-
         customClass.getMethod("printMessage").invoke(customClassInstance);
+
     }
 }
